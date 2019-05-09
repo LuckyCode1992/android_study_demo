b0d25824ca502422b02747cd7232cf9ed85f2a8c
2214576751@qq.com <hxl19920401>
2019-05-09 17:30:31 +0800

    textview部分简单

--- app/src/main/java/com/justcode/hxl/androidstudydemo/MainActivity.kt
+++ app/src/main/java/com/justcode/hxl/androidstudydemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.justcode.hxl.androidstudydemo
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.justcode.hxl.androidstudydemo.textview相关.Demo3Activity
 import com.justcode.hxl.androidstudydemo.跟随手指的小球.Demo1Activity
 import com.justcode.hxl.androidstudydemo.霓虹灯效果.Demo2Activity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         }
         btn_demo2.setOnClickListener {
             skip(Demo2Activity::class.java)
+        }
+        btn_demo3.setOnClickListener {
+            skip(Demo3Activity::class.java)
         }
     }
 
---
+++ app/src/main/java/com/justcode/hxl/androidstudydemo/textview相关/Demo3Activity.kt
@@ -0,0 +1,13 @@
+package com.justcode.hxl.androidstudydemo.textview相关
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.justcode.hxl.androidstudydemo.R
+
+class Demo3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo3)
+    }
+}
