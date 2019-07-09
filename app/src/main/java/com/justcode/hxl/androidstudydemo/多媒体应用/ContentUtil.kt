9a306773f840d8ca2fc4889ce0c7e4587739d103
2214576751@qq.com <hxl19920401>
2019-07-09 11:35:31 +0800

    准备开始多媒体

--- app/src/main/java/com/justcode/hxl/androidstudydemo/多媒体应用/ContentUtil.kt
+++ app/src/main/java/com/justcode/hxl/androidstudydemo/多媒体应用/ContentUtil.kt
@@ -11,10 +11,13 @@ import java.util.ArrayList
 
 
 class ContentUtil(var context: Context) {
+    var contentResover: ContentResolver
 
-    lateinit var contentResover: ContentResolver
-    fun getAllPhoto(itemCallBack: ((MutableList<FileItem>) -> Unit)) {
+    init {
         contentResover = context.contentResolver
+    }
+
+    fun getAllPhoto(itemCallBack: ((MutableList<FileItem>) -> Unit)) {
         val photos = ArrayList<FileItem>()
 
         val projection = arrayOf(
@@ -24,8 +27,6 @@ class ContentUtil(var context: Context) {
         )
         runBlocking {
             val job = GlobalScope.launch {
-
-
                 //    desc 按降序排列
                 //projection 是定义返回的数据，selection 通常的sql 语句，例如  selection=MediaStore.Images.ImageColumns.MIME_TYPE+"=? " 那么 selectionArgs=new String[]{"jpg"};
                 var cursor = contentResover.query(
@@ -63,12 +64,119 @@ class ContentUtil(var context: Context) {
                 cursor!!.close()
 
                 cursor = null
-
             }
             job.join()
             itemCallBack.invoke(photos)
         }
     }
+
+    fun getAllMusic(itemCallBack: ((MutableList<FileItem>) -> Unit)) {
+        val musics = ArrayList<FileItem>()
+
+        val projection = arrayOf(
+            MediaStore.Audio.AudioColumns._ID,
+            MediaStore.Audio.AudioColumns.DATA,
+            MediaStore.Audio.AudioColumns.DISPLAY_NAME
+        )
+
+
+        runBlocking {
+            val job = GlobalScope.launch {
+                var cursor = contentResover.query(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    MediaStore.Audio.AudioColumns.DATE_MODIFIED + " desc"
+                )
+
+                var fileId: String
+
+                var fileName: String
+
+                var filePath: String
+
+                while (cursor!!.moveToNext()) {
+
+                    fileId = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.AudioColumns._ID))
+
+                    fileName = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.AudioColumns.DISPLAY_NAME))
+
+                    filePath = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.AudioColumns.DATA))
+
+                    Log.e("ryze_music", "$fileId -- $fileName -- $filePath")
+
+
+                    val fileItem = FileItem(fileId, filePath, fileName)
+
+                    musics.add(fileItem)
+
+                }
+
+                cursor!!.close()
+
+                cursor = null
+            }
+            job.join()
+            itemCallBack.invoke(musics)
+
+        }
+
+    }
+
+    fun getAllVideo(itemCallBack: ((MutableList<FileItem>) -> Unit)) {
+        val videos = ArrayList<FileItem>()
+        val projection = arrayOf(
+            MediaStore.Video.VideoColumns._ID,
+            MediaStore.Video.VideoColumns.DATA,
+            MediaStore.Video.VideoColumns.DISPLAY_NAME
+        )
+
+        runBlocking {
+            val job = GlobalScope.launch {
+                var cursor = contentResover.query(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    MediaStore.Video.VideoColumns.DATE_MODIFIED + " desc"
+                )
+
+
+                var fileId: String
+
+                var fileName: String
+
+                var filePath: String
+
+                while (cursor!!.moveToNext()) {
+
+                    fileId = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Video.VideoColumns._ID))
+
+                    fileName = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Video.VideoColumns.DISPLAY_NAME))
+
+                    filePath = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Video.VideoColumns.DATA))
+
+
+                    Log.e("ryze_video", "$fileId -- $fileName -- $filePath")
+
+                    val fileItem = FileItem(fileId, filePath, fileName)
+
+                    videos.add(fileItem)
+
+                }
+
+
+                cursor!!.close()
+                cursor = null
+            }
+            job.join()
+            itemCallBack.invoke(videos)
+        }
+
+
+    }
+
 }
 
 
--- app/src/main/java/com/justcode/hxl/androidstudydemo/多媒体应用/MusicActivity.kt
+++ app/src/main/java/com/justcode/hxl/androidstudydemo/多媒体应用/MusicActivity.kt
@@ -1,75 +1,25 @@
 package com.justcode.hxl.androidstudydemo.多媒体应用
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.justcode.hxl.androidstudydemo.R
 import kotlinx.android.synthetic.main.activity_music.*
-import android.content.Intent.CATEGORY_OPENABLE
-import android.content.Intent.ACTION_GET_CONTENT
-import android.database.Cursor
-import android.net.Uri
-import android.util.Log
-import android.widget.Toast
-
 
 
 class MusicActivity : AppCompatActivity() {
     private val FILE_SELECT_CODE = 155
+    var contentUtil: ContentUtil? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        contentUtil = ContentUtil(this)
         setContentView(R.layout.activity_music)
-        btn_chose.setOnClickListener {
-            val intent = Intent(ACTION_GET_CONTENT)
-            intent.setType("audio/*")
-            intent.addCategory(Intent.CATEGORY_OPENABLE)
-            try {
-                startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE)
-            } catch (ex: android.content.ActivityNotFoundException) {
-                // Potentially direct the user to the Market with a Dialog
-                Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show()
-            }
+        btn_get_music.setOnClickListener {
+            contentUtil?.getAllMusic {
 
+            }
         }
     }
 
-    private val TAG = "ChooseFile"
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when (requestCode) {
-            FILE_SELECT_CODE -> if (resultCode == Activity.RESULT_OK) {
-                // Get the Uri of the selected file
-                val uri = data!!.data
-                Log.d(TAG, "File Uri: " + uri!!.toString())
-                // Get the path
-                val path = getPath(this, uri)
-                Log.d(TAG, "File Path: $path")
-                // Get the file instance
-                // File file = new File(path);
-                // Initiate the upload
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
 
-    fun getPath(context: Context, uri: Uri): String? {
-        if ("content".equals(uri.getScheme(), ignoreCase = true)) {
-            val projection = arrayOf("_data")
-            var cursor: Cursor? = null
-            try {
-                cursor = context.getContentResolver().query(uri, projection, null, null, null)
-                val column_index = cursor!!.getColumnIndexOrThrow("_data")
-                if (cursor!!.moveToFirst()) {
-                    return cursor!!.getString(column_index)
-                }
-            } catch (e: Exception) {
-                // Eat it  Or Log it.
-            }
-
-        } else if ("file".equals(uri.getScheme(), ignoreCase = true)) {
-            return uri.getPath()
-        }
-        return null
-    }
 }
